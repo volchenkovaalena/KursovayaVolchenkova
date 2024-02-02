4fd0d2610c004bcb24ff12eb9accf49078404f00
alena <misamistake@>
2024-02-02 05:11:37 +0300

    Did my HW

---
+++ src/Employee.java
@@ -0,0 +1,79 @@
+import java.util.Objects;
+
+public class Employee {
+
+    private String name;
+    private String middleName;
+    private String surname;
+    private int salary;
+    private int department;
+    private int id;
+    private static int counter = 100;
+
+    public Employee ( String name, String middleName, String surname, int salary, int department ) {
+        this.name = name;
+        this.middleName = middleName;
+        this.surname = surname;
+        this.salary = salary;
+        this.department = department;
+        this.id = counter++;
+
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public String getMiddleName () {
+        return middleName;
+    }
+
+    public String getSurname () {
+        return surname;
+    }
+
+    public int getSalary () {
+        return salary;
+    }
+
+    public int getDepartment () {
+        return department;
+    }
+
+    public int getId () {
+        return id;
+    }
+
+    public void setDepartment ( int department ) {
+        this.department = department;
+    }
+
+    public void setSurname ( String surname ) {
+        this.surname = surname;
+    }
+
+    public void setSalary ( int salary ) {
+        this.salary = salary;
+    }
+
+
+    public String toString () {
+        return  "ФИО: " + this.surname+ " " +
+                 this.name+ " " + this.middleName +
+                " Зарплата " + this.salary +
+                " Отдел " + this.department +
+                " id " + this.id;
+    }
+
+
+    public boolean equals ( Object o ) {
+        if ( this == o ) return true;
+        if ( !(o instanceof Employee employee) ) return false;
+        return salary == employee.salary && department == employee.department && id == employee.id && Objects.equals ( name, employee.name ) && Objects.equals ( middleName, employee.middleName ) && Objects.equals ( surname, employee.surname );
+    }
+
+
+    public int hashCode () {
+        return Objects.hash ( name, middleName, surname, salary, department, id );
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,101 @@
 public class Main {
+    static Employee[] EmployeeArray = new Employee[10];
+
     public static void main ( String[] args ) {
+        EmployeeArray[0] = new Employee ( "Иван", "Сергеевич", "Рязанцев", 79000, 1 );
+        EmployeeArray[1] = new Employee ( "Светлана", "Игоревна", "Гурова", 81000, 2 );
+        EmployeeArray[2] = new Employee ( "Игорь", "Михайлович", "Серов", 105000, 1 );
+        EmployeeArray[3] = new Employee ( "Артем", "Денисович", "Макаров", 55000, 3 );
+        EmployeeArray[4] = new Employee ( "Ольга", "Ивановна", "Будкова", 78000, 2 );
+        EmployeeArray[5] = new Employee ( "Ирина", "Борисовна", "Шутова", 125000, 5 );
+        EmployeeArray[6] = new Employee ( "Виктор", "Иванович", "Левин", 99000, 4 );
+        EmployeeArray[7] = new Employee ( "Сергей", "Сергеевич", "Земцов", 45000, 3 );
+        EmployeeArray[8] = new Employee ( "Елена", "Николаевна", "Сергеева", 51000, 3 );
+        EmployeeArray[9] = new Employee ( "Святослав", "Игоревич", "Богдановский", 92000, 5 );
+
+        printEmployee ( );
+        calculateSumSalary ( );
+        calculateMinSalary ( );
+        calculateMaxSalary ( );
+        calculateAverageSalary ( );
+        printNames ();
     }
-}
+
+
+    public static void printEmployee () {
+        for ( int i = 0; i < EmployeeArray.length; i++ ) {
+            System.out.println ( EmployeeArray[i] );
+        }
+    }
+
+    public static void calculateSumSalary () {
+        int sumSalary = 0;
+        for ( int i = 0; i < EmployeeArray.length; i++ ) {
+            sumSalary = sumSalary + EmployeeArray[i].getSalary ( );
+
+
+        }
+        System.out.println ( sumSalary );
+    }
+
+    public static void calculateMinSalary () {
+        int minSalary = EmployeeArray[0].getSalary ( );
+        for ( int i = 0; i < EmployeeArray.length; i++ ) {
+            if ( EmployeeArray[i].getSalary ( ) < minSalary ) {
+                minSalary = EmployeeArray[i].getSalary ( );
+            }
+        }
+        System.out.println ( minSalary );
+
+    }
+
+    public static void calculateMaxSalary () {
+        int maxSalary = EmployeeArray[0].getSalary ( );
+        for ( int i = 0; i < EmployeeArray.length; i++ ) {
+            if ( EmployeeArray[i].getSalary ( ) > maxSalary ) {
+                maxSalary = EmployeeArray[i].getSalary ( );
+            }
+        }
+        System.out.println ( maxSalary );
+
+    }
+
+    public static void calculateAverageSalary () {
+        int sumSalary = 0;
+        int k = 0;
+        int averageSalary;
+        for ( int i = 0; i < EmployeeArray.length; i++ ) {
+            sumSalary = sumSalary + EmployeeArray[i].getSalary ( );
+            k = k + 1;
+            }
+            System.out.println (  sumSalary / k );
+        }
+
+        public static void printNames(){
+            for ( int i = 0; i < EmployeeArray.length; i++ )
+                System.out.println ( EmployeeArray[i].getName () + " " + EmployeeArray[i].getMiddleName () + " " + EmployeeArray[i].getSurname ());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
